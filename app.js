8158fe06ebd3b23b2faf1e121f47206e3c2cee9b
pimentel-melvinmichael <pim.melvinmichael@gmail.com>
2023-07-10 15:15:56 +0800

    Add Environment Vars

---
+++ app.js
@@ -0,0 +1,77 @@
+//jshint esversion:6
+require('dotenv').config();
+const express = require('express');
+const bodyParser = require('body-parser');
+const ejs = require('ejs');
+const mongoose = require("mongoose");
+const encrypt = require("mongoose-encryption");
+const port = 3000;
+
+const app = express();
+
+console.log(process.env.API_KEY);
+
+app.use(express.static('public'));
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({ extended: true }));
+
+mongoose.connect("mongodb://0.0.0.0:27017/userDB");
+
+const userSchema = new mongoose.Schema ({
+    email: String,
+    password: String
+});
+
+
+userSchema.plugin(encrypt, { secret: process.env.SECRET, encryptedFields: ["password"] });
+
+const User = new mongoose.model("User", userSchema);
+
+app.get("/", (req, res) => {
+    res.render("home");
+});
+
+app.get("/login", (req, res) => {
+    res.render("login");
+});
+
+app.get("/register", (req, res) => {
+    res.render("register");
+});
+
+app.post("/register", (req, res) => {
+    const newUser = new User({
+        email: req.body.username,
+        password: req.body.password
+    });
+
+    newUser.save()
+        .then(() => {
+            res.render("secrets");
+        })
+        
+        .catch((err) => {
+            console.log(err);
+        });
+});
+
+app.post("/login", (req, res) => {
+    const username = req.body.username;
+    const password = req.body.password;
+
+    User.findOne({email: username})
+        .then((foundUser) => {
+            if (foundUser.password === password) {
+                res.render("secrets");
+            }
+        })
+        
+        .catch((err) => {
+            console.log(err);
+        });
+});
+
+
+app.listen(port, () => {
+    console.log(`Server started on port ${port}`);
+});
